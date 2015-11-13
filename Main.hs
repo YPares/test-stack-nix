9b69c7bdf63d2600517fab1e9a009e9dd4893241
YPares <yves.pares@gmail.com>
2015-11-13 18:34:58 +0100

    Added PCRE

--- Main.hs
+++ Main.hs
@@ -2,5 +2,8 @@ module Main
     where
 
 import Data.Algebra
+import Text.Regex.PCRE.Light
 
-main = print (1 ^+^ 4 :: Int)
+main = do
+  print (1 ^+^ 4 :: Int)
+  print $ match (compile "plop" []) "plop" []

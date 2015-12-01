28fcda108f25948e64867019ceb89ee6da1add86
YPares <yves.pares@gmail.com>
2015-12-01 12:29:19 +0100

    Added print of arguments, to test escaping

--- Main.hs
+++ Main.hs
@@ -5,7 +5,9 @@ module Main
 
 import Data.Algebra
 import Text.Regex.PCRE.Light
+import System.Environment (getArgs)
 
 main = do
+  getArgs >>= print
   print (1 ^+^ 4 :: Int)
   print $ match (compile "plop" []) "plop" []
